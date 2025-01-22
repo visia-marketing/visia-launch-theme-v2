74d4e45659146211f66d8e8cec5f351cc99fedab
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-01-22 10:53:36 -0500

    moved menu location of standard plugins installer

--- functions.php
+++ functions.php
@@ -160,7 +160,7 @@ function my_theme_register_required_plugins() {
 		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
 		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'parent_slug'  => 'plugins.php',            // Parent menu slug.
 		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
 		'has_notices'  => true,                    // Show admin notices or not.
 		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
@@ -168,7 +168,8 @@ function my_theme_register_required_plugins() {
 		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
 		'message'      => '',                      // Message to output right before the plugins table.
     'strings'      => array(
-      'page_title'  => __( 'Install Standard Plugins', 'theme-slug' ),
+      'page_title'  => __( 'Install Standard Plugins', 'visia' ),
+      'menu_title'  => __( 'Standard Plugins', 'visia' ),
     )
   );
 
