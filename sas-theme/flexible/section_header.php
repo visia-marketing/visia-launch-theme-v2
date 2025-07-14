3a0c896e30a5f171814febad59ea62bd04da4907
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-07-14 17:37:21 -0400

    added cards template and styles

--- sas-theme/flexible/section_header.php
+++ sas-theme/flexible/section_header.php
@@ -2,14 +2,20 @@
 
 <?php if ( get_sub_field('section_heading_title') || get_sub_field('section_heading_intro') ): ?>
 
-        <div class="small-12 columns">
+        <div class="small-12 columns fc-section-header">
+
+          <?php if ( get_sub_field('section_heading') ): ?>
+            <p class="g-section-subtitle"><?php echo get_sub_field('section_heading'); ?></p>
+          <?php endif; ?>
+
           <?php if ( get_sub_field('section_heading_title') ): ?>
-            <h2 class="section-h2"><?php echo get_sub_field('section_heading_title'); ?></h2>
+            <h2><?php echo get_sub_field('section_heading_title'); ?></h2>
           <?php endif; ?>
-          
-          <?php if ( get_sub_field('section_heading_intro') ): ?>
-            <p class="section-p"><?php echo get_sub_field('section_heading_intro'); ?></p>
+
+          <?php if ( get_sub_field('section_heading_text') ): ?>
+            <p class="g-medium-text"><?php echo get_sub_field('section_heading_text'); ?></p>
           <?php endif; ?>
+
         </div>
 
 <?php endif; ?>
