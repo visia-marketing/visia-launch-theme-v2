b0fba2f3ab7c5b1201743732bd57c69882eae43c
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-10-14 13:43:25 -0400

    updated webpack and assets folder structure to separate dist and src

--- lib/setup.php
+++ lib/setup.php
@@ -128,15 +128,15 @@ function assets() {
   //$fonts = 'https://use.typekit.net/xty7yku.css';
 
   wp_enqueue_style('theme-fonts', $fonts , false, null);
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/dist/main.min.css'), false, null);
+  wp_enqueue_style('sage/css', Assets\asset_path('/dist/styles/main.min.css'), false, null);
   wp_enqueue_style('default-css', get_stylesheet_uri() , false, null);
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/dist/main.min.js'), ['jquery'], null, true);
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/dist/main.min.js'), ['jquery'], null, true);
+  wp_enqueue_script('sage/js', Assets\asset_path('dist/scripts/main.min.js'), ['jquery'], null, true);
+  wp_enqueue_script('sage/js', Assets\asset_path('dist/scripts//main.min.js'), ['jquery'], null, true);
 
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
