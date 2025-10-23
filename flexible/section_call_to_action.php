1bd6956f23b304634a8ea69a81853e9b6d3f3535
Randal Pope <randalpope@Randals-MacBook-Air.local>
2025-10-23 10:38:34 -0400

    finalized CTA styles and controls

--- flexible/section_call_to_action.php
+++ flexible/section_call_to_action.php
@@ -2,23 +2,32 @@
 <?php $cta_id = 'call-to-action-' . rand(); ?>
 <?php
 if ( $source === 'default' ){
+
     $background = get_field('cta_background', 'option');
+
     if( $background == 'image'){
+        $content_color = get_field('cta_text_color', 'option') ?? '';
         $bg_image = get_field('cta_background_image', 'option');
         $image = wp_get_attachment_image_src( $bg_image, 'large' );
   
     }
+
     $content = get_field('cta_content', 'option');
     $button = get_field('cta_link', 'option');
 
 }else{
+
     $background = get_sub_field('cta_background');
+   
     if( $background == 'image'){
+        $content_color = get_field('cta_text_color') ?? '';
         $bg_image = get_sub_field('cta_background_image');
         $image = wp_get_attachment_image_src( $bg_image, 'large' );
     }
+
     $content = get_sub_field('cta_content');
     $button = get_sub_field('cta_link');
+
 }
 ?>
 
@@ -32,7 +41,7 @@ if ( $source === 'default' ){
     }
 </style>
 
-<div class="fc-section-cta call-to-action call-to-action--<?php echo $source;?> call-to-action-<?php echo $background; ?> background--<?php echo $background;?>" id="<?php echo $cta_id; ?>">
+<div class="fc-section-cta call-to-action call-to-action--<?php echo $source;?> call-to-action-<?php echo $background; ?> background--<?php echo $background;?> background--<?php echo $content_color;?>" id="<?php echo $cta_id; ?>">
 
     <div class="call-to-action--inner row">
 
